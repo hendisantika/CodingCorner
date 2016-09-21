8d480f5256bd0f0808942015abeee1d7b7d21972
hendisantika <hendisantika@yahoo.co.id>
2016-09-21 10:33:07 +0700

    Palindrome Triangle

---
+++ src/main/java/com/hendi/codingcorner/PalindromeTriangle.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hendi.codingcorner;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author hendisantika
+ */
+public class PalindromeTriangle {
+
+    public static void main(String[] args) {
+        int i, j, n;
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.print("Enter number of lines[height of triangle] : \t");
+            n = scan.nextInt();
+        }
+        for (i = 1; i <= n; i++) {
+            for (j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            for (j = i - 1; j >= 1; j--) {
+                System.out.print(j);
+            }
+            System.out.print("\n");
+        }
+
+    }
+}
