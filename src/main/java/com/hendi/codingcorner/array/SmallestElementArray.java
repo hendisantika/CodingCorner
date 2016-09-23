42685a8c0db072addc30227bc1f9b327e0fa127e
hendisantika <hendisantika@yahoo.co.id>
2016-09-23 17:31:44 +0700

    Smallest Element Array

---
+++ src/main/java/com/hendi/codingcorner/array/SmallestElementArray.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hendi.codingcorner.array;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author hendisantika
+ */
+public class SmallestElementArray {
+
+    public static void main(String[] args) {
+        int i, n;
+        System.out.print("How many numbers ?\t");
+        Scanner scan = new Scanner(System.in);
+        n = scan.nextInt();
+        int array[] = new int[n];
+        for (i = 0; i < n; i++) {
+            System.out.print("\nEnter number " + (i + 1));
+            array[i] = scan.nextInt();
+        }
+        scan.close();
+        for (i = 0; i < n; i++) {
+            if (array[0] > array[i]) {
+                array[0] = array[i];
+            }
+        }
+        System.out.print("\nThe largest among the " + n + " numbers is "
+                + array[0]);
+    }
+}
