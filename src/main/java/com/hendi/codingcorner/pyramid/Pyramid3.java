ba726f6c7e543549967111621499b1071653f2e5
hendisantika <hendisantika@yahoo.co.id>
2016-10-22 05:45:35 +0700

    Pyramid in Java

---
+++ src/main/java/com/hendi/codingcorner/pyramid/Pyramid3.java
@@ -0,0 +1,65 @@
+/*
+        Java Pyramid 3 Example
+        This Java Pyramid example shows how to generate pyramid or triangle like
+        given below using for loop.
+       
+        *
+        **
+        ***
+        ****
+        *****
+        *****
+        ****
+        ***
+        **
+        *
+ */
+package com.hendi.codingcorner.pyramid;
+
+/**
+ *
+ * @author hendisantika
+ */
+public class Pyramid3 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+
+            //generate a new line
+            System.out.println("");
+        }
+
+        //create second half of pyramid
+        for (int i = 5; i > 0; i--) {
+
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+
+            //generate a new line
+            System.out.println("");
+        }
+
+    }
+}
+
+/*
+ 
+Output of the example would be
+*
+**
+***
+****
+*****
+*****
+****
+***
+**
+*
+ 
+ */
