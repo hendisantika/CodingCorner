bd2759284bcb311063c6fbdde6ef5a8b5740454f
hendisantika <hendisantika@yahoo.co.id>
2016-10-24 10:49:10 +0700

    Bubble Sort Asc in Java

--- src/main/java/com/hendi/codingcorner/datastructures/BubbleSortAsc.java
+++ src/main/java/com/hendi/codingcorner/datastructures/BubbleSortAsc.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.hendi.codingcorner.datastructures;
 
 import java.util.Arrays;
@@ -15,14 +10,14 @@ public class BubbleSortAsc {
 
     public static void main(String[] args) {
 
-        int arrayList[] = {5, 3, 9, 7, 1, 8};
-        System.out.println("\nFinal result:" + Arrays.toString(BubbleSortAsc(arrayList)));
+//        int list[] = {5, 3, 9, 7, 1, 8};
+        int[] list = {98, 34, 55, 12, 9, 1, 4, 56,0};
+        System.out.println("Final result:" + Arrays.toString(BubbleSortAsc(list)));
     }
 
     public static int[] BubbleSortAsc(int[] arr) {
         int temp;
         for (int i = 0; i < arr.length - 1; i++) {
-
             for (int j = 1; j < arr.length - i; j++) {
                 if (arr[j - 1] > arr[j]) {
                     temp = arr[j - 1];
@@ -30,7 +25,7 @@ public class BubbleSortAsc {
                     arr[j] = temp;
                 }
             }
-            System.out.println((i + 1) + "th iteration result: " + Arrays.toString(arr));
+            System.out.println((i + 1) + " iteration result: " + Arrays.toString(arr));
         }
         return arr;
     }
