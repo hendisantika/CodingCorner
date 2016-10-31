6a8e8b0b0153d1bf7689302cbe30f91b6a1a68e1
hendisantika <hendisantika@yahoo.co.id>
2016-10-31 19:41:22 +0700

    Upper Triangular Matrix in java

---
+++ src/main/java/com/hendi/codingcorner/matrix/UpperTriangularMatrix.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hendi.codingcorner.matrix;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author hendisantika
+ */
+public class UpperTriangularMatrix {
+
+    public static void main(String[] args) {
+        int rows, columns, i, j;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter order of matrix :  \t");
+        rows = scan.nextInt();
+        columns = scan.nextInt();
+        int matrix[][] = new int[rows][columns];
+        System.out.print("Enter elements of the matrix :\n");
+        for (i = 0; i < rows; i++) {
+            for (j = 0; j < columns; j++) {
+                System.out.print("Enter element m" + (i + 1) + (j + 1) + ":\t");
+                matrix[i][j] = scan.nextInt();
+            }
+        }
+        scan.close();
+        System.out.print("\n\nMATRIX is :\n");
+        for (i = 0; i < rows; i++) {
+            for (j = 0; j < columns; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+
+            System.out.print("\n");
+        }
+
+        System.out.print("\n\nIts Upper Triangular form is: \n");
+        for (i = 0; i < rows; i++) {
+            for (j = 0; j < columns; j++) {
+                if (i <= j) {
+                    System.out.print(matrix[i][j] + "\t");
+                } else {
+                    System.out.print(0 + "\t");
+                }
+            }
+            System.out.print("\n");
+        }
+
+    }
+}
